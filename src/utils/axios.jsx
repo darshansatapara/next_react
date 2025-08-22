d7d7d429c8c408ce44c3aa82d5d7bd2f822c25d8
darshansatapara <darshansatapara286@gmail.com>
2025-08-22 13:18:55 +0530

    update

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -4,6 +4,7 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "true", // incorrect
   },
   withCredentials: true,
 });
