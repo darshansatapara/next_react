c542fb68c62567b57106f7bb2cd16189ec20c0ca
darshansatapara <darshansatapara286@gmail.com>
2025-08-22 11:19:56 +0530

    changes

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  withCredentials: true, // if you use cookies / CORS credentials
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // Attach JWT automatically
